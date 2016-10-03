ca63bcc296c506d04e499d367dd39145c14e9bf4
Michael Perkhofer <michael@perkhofer.at>
2016-10-03 11:46:34 +0200

    Remove unecessary bootstrap code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
